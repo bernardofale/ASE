75ee35e5ba08d287045b23150f20f9357951cf71
Bernardo Falé <mbfale@ua.pt>
2023-06-19 19:54:00 +0100

    New updates and new ultrasonic sensor integration

--- Project/main/main.c
+++ Project/main/main.c
@@ -22,113 +22,90 @@
 #include "lwip/sys.h"
 #include "esp_netif.h"
 #include "cJSON.h"
+#include "driver/gptimer.h"
+#include "rom/ets_sys.h"
+#include "driver/gpio.h"
+#include "esp_timer.h"
 
-#define READ_LEN 256
+#define TRIGGER_GPIO GPIO_NUM_25
+#define ECHO_GPIO GPIO_NUM_26
 
 /* Thread functions prototypes */
-void vibe_acq( void *pvParameters );
-void calc_fft( void *pvParameters );
-void print_seg( void *pvParameters );
+void distance( void *pvParameters );
 void dashboard( void *pvParameters );
 
-/* Semaphore definition */
-SemaphoreHandle_t acq_to_fft;
+/* Timer definitions */
+gptimer_handle_t distance_timer_handle;
 
-/* ADC definitions */
-#if CONFIG_IDF_TARGET_ESP32
-static adc_channel_t channel[1] = {ADC_CHANNEL_7}; /* GPIO 35 */
-#endif
-adc_continuous_handle_t handle = NULL;
-uint8_t result[READ_LEN] = {0};
+static const char *TAG = "Motion Sensor";
 
-static const char *TAG = "Guitar Tuner";
-
-static int freq = 0;
 
 void app_main(void)
 {
-    /* ADC Setup */
-    memset(result, 0xcc, READ_LEN);
-    TaskHandle_t s_task_handle = xTaskGetCurrentTaskHandle();
-    setTaskHandle(s_task_handle);
 
-    continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t), &handle, "Vibration Acquisition");
-
-    adc_continuous_evt_cbs_t cbs = {
-        .on_conv_done = s_conv_done_cb,
-    };
-    ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
-    ESP_ERROR_CHECK(adc_continuous_start(handle));
-
-    /* Display7Seg Setup */
-    display7seg_setup();
-
-    /* WiFi Setup */
+    /* WiFi Setup 
     ESP_ERROR_CHECK(nvs_flash_init());
     wifi_init_sta();
+    */
 
-    /* Semaphore creation */
-    acq_to_fft = xSemaphoreCreateBinary();
-    if(acq_to_fft == NULL)
-    {
-        ESP_LOGI("Semaphore", "Semaphore not created");
-    }
+    /* Distance timer setup */
+    distance_timer_handle = NULL;
+    gptimer_config_t distance_timer_config = {
+        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+        .direction = GPTIMER_COUNT_UP,
+        .resolution_hz = 1 * 1000 * 1000 //1us
+    };
+    ESP_ERROR_CHECK(gptimer_new_timer(&distance_timer_config, &distance_timer_handle));
 
+    /* GPIO definition */
+    gpio_reset_pin(TRIGGER_GPIO);
+    gpio_reset_pin(ECHO_GPIO);
+    gpio_set_direction(TRIGGER_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_direction(ECHO_GPIO, GPIO_MODE_INPUT);    
+    
+    
     /* Task creation */
-    xTaskCreate(&vibe_acq, "Vibration Acquisition", 4096, NULL, tskIDLE_PRIORITY + 4, NULL);
-    xTaskCreate(&calc_fft, "Calculate FFT", 2048, NULL, tskIDLE_PRIORITY + 3, NULL);
-    xTaskCreate(&print_seg, "Print to 7 segments", 2048, NULL, tskIDLE_PRIORITY + 2, NULL);
-    xTaskCreate(&dashboard, "Dashboard", 4096, NULL, tskIDLE_PRIORITY + 4, NULL);
+    xTaskCreate(&distance, "Distance Task", 4096, NULL, tskIDLE_PRIORITY, NULL);
+    //xTaskCreate(&dashboard, "Dashboard", 4096, NULL, tskIDLE_PRIORITY + 4, NULL);
 }
 
-void vibe_acq( void *pvParameters )
-{
-    //ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-    uint16_t ret;
-    uint32_t ret_num = 0;
+void distance( void *pvParameters ){
+    ESP_LOGI("Distance", "Start operations");
+    uint16_t echo_duration = 0;
 
-    int cnt = 0;
-    int med = 0;
     while(1)
     {
-        med = 0;
-        ESP_LOGI("Vibration Acquisition", "Reading Value");
-        ret = adc_continuous_read(handle, result, READ_LEN, &ret_num, 0);
-        if(ret == ESP_OK){
-            //ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32, ret, ret_num);
-            for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
-                adc_digi_output_data_t *p = (void*)&result[i];
-                med += p->type1.data;
-            }
-            freq = (int) med*SOC_ADC_DIGI_RESULT_BYTES/ret_num;
-            if(cnt == 4){
-                cnt = -1;
-                ESP_LOGI("Vibration Acquisition", "Unit: %d, Channel: %d, Value: %d", 1, 7, freq);
-            }
-        }
-        cnt++;
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-}
+        
+        /* Sending pulse to activate sensor trigger */
+        gpio_set_level(TRIGGER_GPIO, 1);
+        ets_delay_us(10);
+        gpio_set_level(TRIGGER_GPIO, 0);
 
-void calc_fft( void *pvParameters )
-{
-    while(1)
-    {
-        ESP_LOGI("Calculate FFT", "Start operations");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
+        /* Wait until echo signals reception */
+        int level = gpio_get_level(ECHO_GPIO);
+        while (level == 0) level = gpio_get_level(ECHO_GPIO);
 
-void print_seg( void *pvParameters )
-{
-    ESP_LOGI("Print to 7 segments", "Start operations");
-    int cnt = 0;
-    while(1) {
-        if(cnt == 7) cnt = 0;
-        display(TAG, cnt);
-        vTaskDelay(3000 / portTICK_PERIOD_MS); 
-        cnt++;
+        /* -> Echo starts reception
+           -> Start timer to count the duration of the echo 
+        */
+        ESP_ERROR_CHECK(gptimer_enable(distance_timer_handle));
+        ESP_ERROR_CHECK(gptimer_start(distance_timer_handle));
+
+        while (level == 1) level = gpio_get_level(ECHO_GPIO);
+
+        /* Get the high precision timer in us */
+        ESP_ERROR_CHECK(gptimer_get_raw_count(distance_timer_handle, &echo_duration));
+
+        /* Calculate the distance based on the duration of the echo */
+        float distance = (float)echo_duration * 0.034 / 2.0;
+        printf("Distance: %.2f cm\n", distance);
+        
+        /* Stop and reset the timer */
+        ESP_ERROR_CHECK(gptimer_stop(distance_timer_handle));
+        ESP_ERROR_CHECK(gptimer_set_raw_count(distance_timer_handle, 0));
+        ESP_ERROR_CHECK(gptimer_disable(distance_timer_handle));
+
+        vTaskDelay(70 / portTICK_PERIOD_MS);
     }
 }
 
@@ -138,13 +115,7 @@ void dashboard( void *pvParameters )
     const char chars[] = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G'
     };
-    int i = 0;
     while(1)
-    {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        
-        if(i == 7) i = 0;
-        tcp_client("Dashboard", "oi");
-        i++;
+    {   
     }
 }
